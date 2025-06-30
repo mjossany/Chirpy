5a55b5e9dcceb0c64203cf399b1ba72e6634ce3b
Jossany Silva <mjossany@gmail.com>
2025-06-30 16:24:20 -0300

    feat(api): add endpoint for chirp creation
    Introduces `POST /api/chirps` to create and save new chirps. Includes the necessary handler, database query, and migration.

---
+++ internal/api/chirp.go
@@ -0,0 +1,85 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mjossany/Chirpy/internal/database"
+)
+
+type createChirpRequest struct {
+	Body   string    `json:"body"`
+	UserId uuid.UUID `json:"user_id"`
+}
+
+type createChirpResponse struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserId    uuid.UUID `json:"user_id"`
+}
+
+func (cfg *Config) handleCreateChirp(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var request createChirpRequest
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&request)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	validateChirp(request, w)
+	cleanedBody := bodyCleaning(request.Body)
+
+	now := time.Now().UTC()
+	chirp, err := cfg.DB.CreateChirp(r.Context(), database.CreateChirpParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Body:      cleanedBody,
+		UserID:    request.UserId,
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating chirp", err)
+		return
+	}
+
+	resp := createChirpResponse{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserId:    chirp.UserID,
+	}
+	respondWithJSON(w, http.StatusCreated, resp)
+}
+
+func validateChirp(request createChirpRequest, w http.ResponseWriter) {
+	const maxChirpLength = 140
+	if len(request.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+}
+
+func bodyCleaning(chirpy string) string {
+	forbiddenWords := []string{"kerfuffle", "sharbert", "fornax"}
+	splittedChirpy := strings.Split(chirpy, " ")
+	for index, word := range splittedChirpy {
+		if slices.Contains(forbiddenWords, strings.ToLower(word)) {
+			splittedChirpy[index] = "****"
+		}
+	}
+	return strings.Join(splittedChirpy, " ")
+}
--- internal/api/server.go
+++ internal/api/server.go
@@ -18,8 +18,8 @@ func NewRouter(cfg *Config, filepathRoot string) http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/api/healthz", handlerReadiness)
-	mux.HandleFunc("/api/validate_chirp", handleChirpValidation)
 	mux.HandleFunc("/api/users", cfg.handleCreateUser)
+	mux.HandleFunc("/api/chirps", cfg.handleCreateChirp)
 	mux.HandleFunc("/admin/metrics", cfg.handlerMetrics)
 	mux.HandleFunc("/admin/reset", cfg.handlerReset)
 	return mux
