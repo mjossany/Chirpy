f483f7639114fed08d4af2e3db0b2c126a97a8f7
Jossany Silva <jossanysilva@CQ57W1RFVG.local>
2025-07-24 06:39:29 -0300

    feat: minor logs

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	serverMux := http.NewServeMux()
 
 	const port = "8080"
+	const filepathRoot = "."
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: serverMux,
 	}
 
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+	serverMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
-	srv.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
